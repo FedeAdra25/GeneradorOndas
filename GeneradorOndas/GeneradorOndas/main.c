f9d7f08959a79dea66fef61eb8d150b935a3efa7
FedeAdra25 <fedeadra@gmail.com>
2021-06-05 15:29:13 -0300

    first commit

---
+++ GeneradorOndas/GeneradorOndas/main.c
@@ -0,0 +1,18 @@
+/*
+ * GeneradorOndas.c
+ *
+ * Created: 05-Jun-21 3:26:02 PM
+ * Author : fedea
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
